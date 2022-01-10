822d588ea4522dabb002bc6fbebe31e48532bd29
Abhishek <abhishek.rai@nubiz.in>
2022-01-10 14:02:40 +0530

    df

--- core/settings.py
+++ core/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES={ 'default': dj_database_url.config() }
 
 DATABASES = {
     'default': {
